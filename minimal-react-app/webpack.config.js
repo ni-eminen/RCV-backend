79e768b82367034257c4ddf6a06a1b07caaaa600
ni-eminen <matiasnie2@gmail.com>
2022-04-14 13:23:47 +0300

    compression

--- minimal-react-app/webpack.config.js
+++ minimal-react-app/webpack.config.js
@@ -1,23 +1,35 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+var CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: './app.js'
+    filename: "./app.js",
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: 'babel-loader'
-    }]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: 'src/index.html' })
+    new HtmlWebpackPlugin({ template: "src/index.html" }),
+    new webpack.DefinePlugin({
+      // <-- key to reducing React's size
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin(), //minify everything
+    new webpack.optimize.AggressiveMergingPlugin(), //Merge chunks
   ],
   devServer: {
-    host: '0.0.0.0',
-    historyApiFallback: true
+    host: "0.0.0.0",
+    historyApiFallback: true,
   },
-  mode: 'none'
-}
+  mode: "none",
+};
